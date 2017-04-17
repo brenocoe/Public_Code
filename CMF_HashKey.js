8b98d205b569b75a5e9d91e8b0fd73a21083df4a
brenocoe <breno.coe@gmail.com>
2017-04-17 12:56:49 -0300

    Update CMF_HashKey.js

--- CMF_HashKey.js
+++ CMF_HashKey.js
@@ -1,4 +1,4 @@
-//alert('Caro usuario, os licenciamentos das aplicacoes de PROTESTO DE TITULOS e RECONHECIMENTO DE FIRMAS devem ser renovados ate o dia 10 de cada mes. Favor entrar em contato com Breno Carvalho Coe - breno.coe@gmail.com - (85) 9.9725-3069.');
+alert('Caro usuario, os licenciamentos das aplicacoes de PROTESTO DE TITULOS e RECONHECIMENTO DE FIRMAS devem ser renovados ate o dia 10 de cada mes. Favor entrar em contato com Breno Carvalho Coe - breno.coe@gmail.com - (85) 9.9725-3069.');
 
 //alert('O LOG de eventos das aplicações precisa de manutenção preventiva. Favor entrar em contato com Breno Carvalho Coe - breno.coe@gmail.com - (85) 9.9725-3069.');
 
